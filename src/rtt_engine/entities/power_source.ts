cf957664385771eb6af75df5e43835361b016ecd
Michael Mokrysz <michael.mokrysz@digital.cabinet-office.gov.uk>
2020-01-24 21:15:54 +0000

    Improve constructor of PowerSource
    It now sets `collisionRadius` for you and is more consistent with the
    other units.

--- src/rtt_engine/entities/power_source.ts
+++ src/rtt_engine/entities/power_source.ts
@@ -1,4 +1,12 @@
+import { Vector } from '../vector';
 import { PowerGenerator } from './power_generator';
 import { PointOfInterest } from './lib/point_of_interest';
 
-export class PowerSource extends PointOfInterest<PowerGenerator> {}
+export class PowerSource extends PointOfInterest<PowerGenerator> {
+    constructor(position: Vector) {
+        super({
+            position,
+            collisionRadius: 7.0,
+        })
+    }
+}
