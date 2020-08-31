ab8d18105e46e9a3d8b140aeb5e5735d5a647b93
Michael Mokrysz <hi@46b.it>
2020-08-31 02:02:24 +0100

    Switch to an Orthographic camera
    Until now I've been a bit lazy and used a perspective camera. The
    perspective distortion was small enough to be unnoticable in what is
    still a 2D game. But as I move into more complex visual effects it has
    become a problem.
    
    This commit switches to an orthographic camera. For the time being it
    also removes the ability to zoom in/out and move the camera around.
    That isn't essential and I can reintroduce it as part of improving the
    user interaction. :)

--- src/rtt_threejs_renderer/renderer.ts
+++ src/rtt_threejs_renderer/renderer.ts
@@ -1,7 +1,4 @@
 import * as THREE from 'three';
-import CameraControls from 'camera-controls';
-
-CameraControls.install({ THREE: THREE });
 
 export function time(name: string, callback: () => void): void {
   let clock = new THREE.Clock();
@@ -12,7 +9,10 @@ export function time(name: string, callback: () => void): void {
 
 export class Renderer {
   clock: THREE.Clock;
-  camera: THREE.PerspectiveCamera;
+  border: number;
+  worldSize: number;
+  aspect: number;
+  camera: THREE.OrthographicCamera;
   scene: THREE.Scene;
   gameCoordsGroup: THREE.Group;
   renderer: THREE.WebGLRenderer;
@@ -20,41 +20,51 @@ export class Renderer {
 
   constructor(worldSize: number, window: any, document: any) {
     this.clock = new THREE.Clock();
-    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, worldSize * 200);
-    //this.camera.position.z = worldSize;
+
+    this.worldSize = worldSize;
+    this.aspect = window.innerWidth / window.innerHeight;
+    // FIXME: Relate this to the window size vs the world size so it is constant and in pixels
+    this.border = 20;
+    this.camera = new THREE.OrthographicCamera(
+      -this.worldSize / 2 * this.aspect - this.border,
+      this.worldSize / 2 * this.aspect + this.border,
+      this.worldSize / 2 + this.border,
+      -this.worldSize / 2 - this.border,
+      0.01, this.worldSize * 10,
+    );
+    this.camera.position.z = this.worldSize;
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0x202020);
     this.gameCoordsGroup = new THREE.Group();
-    this.gameCoordsGroup.position.x = -worldSize / 2;
-    this.gameCoordsGroup.position.y = -worldSize / 2;
+    this.gameCoordsGroup.position.x = -this.worldSize / 2;
+    this.gameCoordsGroup.position.y = -this.worldSize / 2;
     this.scene.add(this.gameCoordsGroup);
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       alpha: true,
-      powerPreference: "high-performance"
+      powerPreference: "high-performance",
     });
     if (window.devicePixelRatio != null) {
       this.renderer.setPixelRatio(window.devicePixelRatio);
     }
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
-    this.controls = new CameraControls(this.camera, this.renderer.domElement);
-    this.controls.minDistance = 5;
-    this.controls.maxDistance = worldSize;
-    this.controls.dollyToCursor = true;
-    this.controls.mouseButtons.left = CameraControls.ACTION.DOLLY;
-    this.controls.mouseButtons.right = CameraControls.ACTION.TRUCK;
-    this.controls.mouseButtons.wheel = CameraControls.ACTION.DOLLY;
-    // FIXME: Need to actually try these options out
-    this.controls.touches.one = CameraControls.ACTION.TOUCH_TRUCK;
-    this.controls.touches.two = CameraControls.ACTION.TOUCH_DOLLY;
-    this.controls.touches.three = CameraControls.ACTION.TOUCH_DOLLY_TRUCK;
-    this.controls.setLookAt(0, 0, worldSize, 0, 0, 0, false);
   }
 
   animate(force = false) {
-    this.controls.update(this.clock.getDelta());
+    //this.controls.update(this.clock.getDelta());
     requestAnimationFrame(() => this.animate());
+
+    let aspect = window.innerWidth / window.innerHeight;
+    if (aspect != this.aspect) {
+      this.aspect = aspect;
+      this.camera.left = -this.worldSize / 2 * this.aspect - this.border;
+      this.camera.right = this.worldSize / 2 * this.aspect + this.border;
+      this.camera.top = this.worldSize / 2 + this.border;
+      this.camera.bottom = -this.worldSize / 2 - this.border;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
 
     time("animate", () => {
       this.renderer.render(this.scene, this.camera);
