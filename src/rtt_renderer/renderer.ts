ae63c303802ab57747c9f9de467331e4f15d0e53
Michael Mokrysz <hi@46b.it>
2020-09-03 04:45:09 +0100

    Use arrow keys to move the map when zoomed in
    This adds the last basic map interaction needed for mouse/keyboard
    devices. Zooming and selecting units have both alreadu been
    implemented. That only leaves support for mobile devices.

--- src/rtt_renderer/renderer.ts
+++ src/rtt_renderer/renderer.ts
@@ -19,6 +19,7 @@ export class Renderer {
   gameCoordsGroup: THREE.Group;
   renderer: THREE.WebGLRenderer;
   screenPositionToWorldPosition: ScreenPositionToWorldPosition;
+  pressedArrowKeys: Set<number>;
 
   constructor(worldSize: number, window: any, document: any) {
     this.clock = new THREE.Clock();
@@ -55,8 +56,11 @@ export class Renderer {
     document.body.appendChild(this.renderer.domElement);
 
     this.screenPositionToWorldPosition = new ScreenPositionToWorldPosition(this.renderer.domElement, this.camera);
+    this.pressedArrowKeys = new Set();
 
     this.renderer.domElement.addEventListener("wheel", (e) => this.wheel(e), false);
+    window.addEventListener("keydown", (e) => this.keydown(e), false);
+    window.addEventListener("keyup", (e) => this.keyup(e), false);
   }
 
   animate(force = false) {
@@ -75,6 +79,33 @@ export class Renderer {
     // }
 
     time("animate", () => {
+      let dx = 0;
+      let dy = 0;
+      this.pressedArrowKeys.forEach((keyCode) => {
+        switch (keyCode) {
+          case 37: // left arrow key
+            dx += 1;
+            break;
+          case 39: // right arrow key
+            dx -= 1;
+            break;
+          case 38: // up arrow key
+            dy += 1;
+            break;
+          case 40: // down arrow key
+            dy -= 1;
+            break;
+        }
+      });
+      if (dx != 0 || dy != 0) {
+        // FIXME: I'm not convinced this calculation works, but the UI result is OK
+        // FIXME: Make these movements smoother. Acceleration/easing.
+        const neededMovementForOnePixelAtThisScrollLevel = 15 / this.camera.matrix.elements[0];
+        const translate = new THREE.Matrix4().makeTranslation(-dx * neededMovementForOnePixelAtThisScrollLevel, dy * neededMovementForOnePixelAtThisScrollLevel, 0);
+        this.camera.applyMatrix4(translate);
+        this.moveWithinBounds();
+      }
+
       this.renderer.render(this.scene, this.camera);
       //console.debug("draw calls: " + this.renderer.info.render.calls);
     });
@@ -99,6 +130,21 @@ export class Renderer {
     this.camera.applyMatrix4(scaleWorldAroundGameMouse);
     this.camera.applyMatrix4(translateGameMouseBack);
 
+    this.moveWithinBounds();
+  }
+
+  keydown(event: {keyCode: number}) {
+    if ([37, 38, 39, 40].includes(event.keyCode)) {
+      this.pressedArrowKeys.add(event.keyCode);
+    }
+  }
+
+  keyup(event: {keyCode: number}) {
+    this.pressedArrowKeys.delete(event.keyCode);
+  }
+
+  // FIXME: Need to rescale if zoom level is wrong
+  moveWithinBounds() {
     // Prevent zoom from going outside the game board at the left or top
     // FIXME: Try to encapsulate this logic
     const transformedTopLeft = new THREE.Vector4(-this.worldSize/2, this.worldSize/2).applyMatrix4(this.camera.matrix);
