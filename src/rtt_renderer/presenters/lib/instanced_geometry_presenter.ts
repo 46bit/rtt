339953c5ee4b1c350991f1f5c265069521660e82
Michael Mokrysz <hi@46b.it>
2020-09-05 00:13:41 +0100

    Fix instanced geometry memory leaking

--- src/rtt_renderer/presenters/lib/instanced_geometry_presenter.ts
+++ src/rtt_renderer/presenters/lib/instanced_geometry_presenter.ts
@@ -3,12 +3,12 @@ import { Player } from '../../../rtt_engine/player';
 import { IMovable } from '../../../rtt_engine/entities';
 import { Vector } from '../../../rtt_engine/vector';
 
-export class InstancedGeometryPresenter {
+export abstract class InstancedGeometryPresenter {
   singleGeometry: THREE.BufferGeometry;
   material: THREE.Material;
   scene: THREE.Group;
-  attributes: [key: string]: Float32Array;
-  instancedGeometry?: THREE.InstancedMesh;
+  attributes: {[key: string]: Float32Array};
+  instancedGeometry?: THREE.InstancedBufferGeometry;
   mesh?: THREE.Mesh;
 
   constructor(singleGeometry: THREE.BufferGeometry, material: THREE.Material, scene: THREE.Group) {
@@ -18,7 +18,7 @@ export class InstancedGeometryPresenter {
     this.attributes = {};
   }
 
-  abstract getInstances(): {position: Vector, direction: number, player}[] {}
+  abstract getInstances(): {position: Vector, direction: number, player: {color: THREE.Color}}[];
 
   predraw(instances: any[]) {
     const numberOfInstances = instances.length;
@@ -60,20 +60,21 @@ export class InstancedGeometryPresenter {
       const instance = instances[i];
       this.attributes.position[i*2] = instance.position.x;
       this.attributes.position[i*2 + 1] = instance.position.y;
-      const facingDirection = instance.turret ? instance.turret.rotation : instance.direction;
+      const facingDirection = instance.turret ? instance.turret!.rotation : instance.direction;
       this.attributes.rotation[i] = Math.PI/2 + facingDirection;
       this.attributes.playerColor[i*3] = instance.player.color.r;
       this.attributes.playerColor[i*3 + 1] = instance.player.color.g;
       this.attributes.playerColor[i*3 + 2] = instance.player.color.b;
     }
-    this.instancedGeometry.getAttribute("instancePosition").needsUpdate = true;
-    this.instancedGeometry.getAttribute("instanceRotation").needsUpdate = true;
-    this.instancedGeometry.getAttribute("playerColor").needsUpdate = true;
+    this.instancedGeometry!.getAttribute("instancePosition").needsUpdate = true;
+    this.instancedGeometry!.getAttribute("instanceRotation").needsUpdate = true;
+    this.instancedGeometry!.getAttribute("playerColor").needsUpdate = true;
   }
 
   dedraw() {
     if (this.instancedGeometry) {
-      this.scene.remove(this.mesh);
+      this.scene.remove(this.mesh!);
+      this.instancedGeometry?.dispose();
       this.mesh = undefined;
       this.instancedGeometry = undefined;
       this.attributes = {};
